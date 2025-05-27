a3bbf463c6459f21c94cd21ba1d246521c78b6d8
acrobaticcow <singed8@gmail.com>
2025-05-27 23:27:04 +0700

    log tmp 3

--- api/log_temp.js
+++ api/log_temp.js
@@ -4,7 +4,7 @@ import os from "os";
 export default async function getTmpContents(req, res) {
   try {
     const files = await fs.promises.readdir(os.tmpdir());
-    res.status(200).send(files);
+    res.status(200).send(`Files are ${files}`);
   } catch (error) {
     console.error("Error reading /tmp:", error);
   }
